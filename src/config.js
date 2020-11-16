bf1d11964d4676e251efe4a90d537789cb6fad78
Peter Nordman <peter.m.nordman@gmail.com>
2020-11-16 08:26:34 -0500

    Removed link to react logo in public/index.html. Refactor src/config to set API endpoint based on .env.

--- src/config.js
+++ src/config.js
@@ -1,8 +1,11 @@
 const config = {
-  API_ENDPOINT: "http://localhost:8000/api",
-  API_KEY: process.env.REACT_APP_FANCYPLANTS_API_KEY,
+  API_ENDPOINT:
+    process.env.REACT_APP_ENVIRONMENT === "development"
+      ? "http://localhost:8000/api"
+      : process.env.REACT_APP_API_ENDPOINT,
+  API_KEY: process.env.REACT_APP_API_KEY,
   TREFLE_API_KEY: process.env.TREFLE_API_KEY,
-  TOKEN_KEY: `Authorization`,
+  TOKEN_KEY: "Authorization",
 };
 
 export default config;
