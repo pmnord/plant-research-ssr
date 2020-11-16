304aa99b7b538d6b0d31fdeb8539a6ca3264fb0f
Peter Nordman <peter.m.nordman@gmail.com>
2020-11-16 13:49:59 -0500

    Restyle PlantSearchListItem

--- src/components/PlantSearchListItem/PlantSearchListItem.js
+++ src/components/PlantSearchListItem/PlantSearchListItem.js
@@ -48,17 +48,25 @@ export default function PlantSearchListItem(props) {
           src={props.image_url}
           alt={props.scientific_name}
         />
-      ) : <div>Image not available</div>}
+      ) : <div>Image not yet available</div>}
 
       <div className="plant-search__plant-names">
-        <h3>
-          <strong>{props.scientific_name}</strong>
-        </h3>
-        {props.common_name ? <p>'{capitalize(props.common_name)}'</p> : <p></p>}
-      </div>
-      <div>
-        <p>{props.family}</p>
-        <p>{props.family_common_name}</p>
+        <div>
+          <p className="plant-names__label">Scientific Name</p>
+          <p className="plant-names__name">{props.scientific_name}</p>
+        </div>
+        <div>
+          <p className="plant-names__label">Family</p>
+          <p className="plant-names__name">{props.family}</p>
+        </div>
+        <div>
+          <p className="plant-names__label">Common Name</p>
+          <p className="plant-names__name">{props.common_name}</p>
+        </div>
+        <div>
+          <p className="plant-names__label">Family Common Name</p>
+          <p className="plant-names__name">{props.family_common_name}</p>
+        </div>
       </div>
       <div className="plant-search__list-item__buttons">
         <Link to={`/plant/${props.trefle_id}`}>
